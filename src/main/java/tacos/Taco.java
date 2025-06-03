32f075f49ece152b4a3353b0b5453d6271a102be
dddmadison <119731457+dddmadison@users.noreply.github.com>
2025-06-04 02:45:37 +0900

    Add files via upload

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,25 +1,24 @@
 package tacos;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-@Entity
+@Document(collection = "taco") // MongoDB 컬렉션 이름 (선택)
 public class Taco {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;  // ObjectId는 String 타입으로 매핑
 
     private String name;
 
     private Date createdAt = new Date();
 
-    @ElementCollection
-    private List<String> ingredients;
+    private List<String> ingredients; // 자동으로 Array 형태로 저장됨
 }
